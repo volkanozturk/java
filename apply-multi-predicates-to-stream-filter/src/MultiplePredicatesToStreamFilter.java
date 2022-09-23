8fdeb9159a99dc0264e42db7b7fbbdce8498898e
volkanozturk <volkans66@hotmail.com>
2022-09-23 21:03:33 +0200

    added Multiple predicates to stream filter

--- apply-multi-predicates-to-stream-filter/src/MultiplePredicatesToStreamFilter.java
+++ apply-multi-predicates-to-stream-filter/src/MultiplePredicatesToStreamFilter.java
@@ -19,30 +19,15 @@ public class MultiplePredicatesToStreamFilter {
 		Predicate<Integer> myPredicatesWithOr = myPredicateList.stream().reduce(Predicate::or).orElse(p -> false);
 		Predicate<Integer> myPredicatesWithAnd = myPredicateList.stream().reduce(Predicate::and).orElse(p -> true);
 
-		final String myEvenIntegersOrGreaterThan5WithReducedPredicates = myIntegerList.stream()
-				.filter(myPredicatesWithOr)
-				.map(Object::toString)
-				.collect(Collectors.joining(", "));
+		final String myEvenIntegersOrGreaterThan5WithReducedPredicates = myIntegerList.stream().filter(myPredicatesWithOr).map(Object::toString).collect(Collectors.joining(", "));
 
-		final String myEvenIntegersOrGreaterThan5WithPredicateOperators = myIntegerList.stream()
-				.filter(isEven.or(isGreaterThan5))
-				.map(Object::toString)
-				.collect(Collectors.joining(", "));
+		final String myEvenIntegersOrGreaterThan5WithPredicateOperators = myIntegerList.stream().filter(isEven.or(isGreaterThan5)).map(Object::toString).collect(Collectors.joining(", "));
 
-		final String myEvenIntegersAndGreaterThan5WithReducedPredicates = myIntegerList.stream()
-				.filter(myPredicatesWithAnd)
-				.map(Object::toString)
-				.collect(Collectors.joining(", "));
+		final String myEvenIntegersAndGreaterThan5WithReducedPredicates = myIntegerList.stream().filter(myPredicatesWithAnd).map(Object::toString).collect(Collectors.joining(", "));
 
-		final String myEvenIntegersAndGreaterThan5WithPredicateOperators = myIntegerList.stream()
-				.filter(isEven.and(isGreaterThan5))
-				.map(Object::toString)
-				.collect(Collectors.joining(", "));
+		final String myEvenIntegersAndGreaterThan5WithPredicateOperators = myIntegerList.stream().filter(isEven.and(isGreaterThan5)).map(Object::toString).collect(Collectors.joining(", "));
 
-		final String myOddIntegersAndSmallerThanOrEqualTo5WithPredicateOperators = myIntegerList.stream()
-				.filter(isEven.negate().and(isGreaterThan5.negate()))
-				.map(Object::toString)
-				.collect(Collectors.joining(", "));
+		final String myOddIntegersAndSmallerThanOrEqualTo5WithPredicateOperators = myIntegerList.stream().filter(isEven.negate().and(isGreaterThan5.negate())).map(Object::toString).collect(Collectors.joining(", "));
 
 		System.out.println(String.format("Even Integers or Integers Greater than 5 are with reduced predicates; %s", myEvenIntegersOrGreaterThan5WithReducedPredicates));
 		//  Even Integers or Integers Greater Than 5 are; 2, 4, 6, 7, 8, 9, 10
